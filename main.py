babd77dfd894d05fe2835ee999149fb8e4b1afc0
AndriiKot <a_kocyuba@ukr.net>
2024-11-26 13:34:25 +0100

    App: 'SmartMatch' 'feature-br' ! task 2: testing limit words  !

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 import glob
 
 data_folder = 'data/'
+files_source = "*.txt"
+max_words = 2
 
 songs = {}
-for filepath in glob.glob(os.path.join(data_folder, '*.txt')):
+for filepath in glob.glob(os.path.join(data_folder, files_source)):
     with open(filepath, 'r', encoding='utf-8') as f:
         text = f.read()
         song_name = os.path.basename(filepath)
@@ -12,5 +14,7 @@ for filepath in glob.glob(os.path.join(data_folder, '*.txt')):
 
 processed_songs = {}
 for song_name, text in songs.items():
-    print(f"Обработка песни: {song_name}")
-    print(f"Текст песни: {text}")
+    processed_text = ' '.join(text.split()[:max_words])
+    processed_songs[song_name] = processed_text
+
+print(processed_songs)
