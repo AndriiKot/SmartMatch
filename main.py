bce41d911e4a106ae9c43aed8e76201da2dfca56
AndriiKot <a_kocyuba@ukr.net>
2024-11-26 12:35:57 +0100

    App: 'SmartMatch' ! Reading all files in the specified folder (data/) !

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(f"Hello world")
+import os
+import glob
+
+data_folder = 'data/'
+
+songs = {}
+for filepath in glob.glob(os.path.join(data_folder, '*.txt')):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        text = f.read()
+        song_name = os.path.basename(filepath)
+        songs[song_name] = text
+
+print("\n\nFile reading completed. Found songs:", list(songs.keys()), "\n\n")
+print(songs)
