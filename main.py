da6be5beb934f01e39e5a1831bf208eedcf189f1
AndriiKot <a_kocyuba@ukr.net>
2024-11-26 14:57:56 +0100

    App: 'SmartMatch' 'vectors-br' ! task 3 step 1 finish vectors !

--- main.py
+++ main.py
@@ -16,6 +16,3 @@ model_name = "distilbert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
 
-print("Model name:", model_name)
-print("Number of songs:", len(songs))
-print("Number of tokens:", sum(len(tokenizer.encode(song)) for song in songs.values()))
