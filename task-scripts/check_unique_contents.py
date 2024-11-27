f98c79720968856570d94fa6a49be7df2ca528c1
AndriiKot <a_kocyuba@ukr.net>
2024-11-27 10:50:43 +0100

    App: 'SmartMatch'  ! delete not unique content and update script 'check_unique_contents' !

--- task-scripts/check_unique_contents.py
+++ task-scripts/check_unique_contents.py
@@ -15,11 +15,11 @@ def check_unique_contents(directory):
                 normalized_content = normalize_content(content)
 
                 if normalized_content in content_dict:
-                    print(f'Содержимое файла "{filename}" не уникально. Повторяется с файлом "{content_dict[normalized_content]}".')
+                    print(f'The content of the file "{filename}" is not unique. It duplicates the content from the file "{content_dict[normalized_content]}".')
                 else:
                     content_dict[normalized_content] = filename
 
-    print("Проверка завершена.")
+    if len(content_dict) == len(os.listdir(directory)):
+        print("Check complete, all files are unique.")
 
 check_unique_contents('../data/')
-
