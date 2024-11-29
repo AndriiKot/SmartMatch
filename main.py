9d44af659b2b2cea71b58033557960163f4ee3d4
AndriiKot <a_kocyuba@ukr.net>
2024-11-29 23:25:40 +0100

    App: 'SmartMatch'! optimization: call get_vectors() one time !

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-# from utils.parse_arguments import parse_arguments
 from utils import load_texts, get_vector, parse_arguments
 
 
-def find_similar_text(user_input, vectors, text_names, model, tokenizer, max_words):
-    user_vector = get_vector(model, tokenizer, user_input, max_words)
+def find_similar_text(user_vector, vectors, text_names):
     similarities = cosine_similarity([user_vector], vectors)
     most_similar_index = np.argmax(similarities)
     most_similar_text = text_names[most_similar_index]
@@ -28,5 +26,6 @@ vectors = np.array([get_vector(model, tokenizer, text, args.max_words) for text 
 text_names = list(texts.keys())
 
 user_input = input("Please enter the text you want to find a similar document for: ")
-similar_text_name, similarity_score = find_similar_text(user_input, vectors, text_names, model, tokenizer, args.max_words)
+user_vector = get_vector(model, tokenizer, user_input, args.max_words)
+similar_text_name, similarity_score = find_similar_text(user_vector, vectors, text_names)
 print(f"The most similar document is: {similar_text_name} with a similarity score of {similarity_score:.4f}")
