8fd730fd202540a0326ad153309b190066ceda00
AndriiKot <a_kocyuba@ukr.net>
2024-12-01 00:45:39 +0100

    App: 'SmartMatch' 'OOP-style' ! test args 'TextSimilarityFinder' !

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ from sklearn.metrics.pairwise import cosine_similarity
 from utils import load_texts, get_vector, parse_arguments
 
 class TextSimilarityFinder:
-    def __init__(self):
+    def __init__(self, model_name, data_folder, files_source, max_words):
         print("Initializing TextSimilarityFinder...", self)
+        print("Model name:", model_name)
+        print("Data folder:", data_folder)
+        print("Files source:", files_source)
+        print("Max words:", max_words)
 
-
-similarity_finder = TextSimilarityFinder()
+if __name__ == "__main__":
+    args = parse_arguments()
+    similarity_finder = TextSimilarityFinder(
+        model_name="distilbert-base-uncased",
+        data_folder=args.data_folder,
+        files_source=args.files_source,
+        max_words=args.max_words
+    )
