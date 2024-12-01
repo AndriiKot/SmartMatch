a487b4d4a3a56a3bce2a8560942baf736c9dc77a
AndriiKot <a_kocyuba@ukr.net>
2024-12-01 22:59:56 +0100

    App: 'SmartMatch' 'faiss' ! replace 'scikit-learn' to 'faiss' !

--- utils/similarity_calculator.py
+++ utils/similarity_calculator.py
@@ -1,14 +1,20 @@
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
+import faiss
 
 class SimilarityCalculator:
     def __init__(self, vectors, text_names):
-        self.vectors = np.array(vectors)  # Преобразуем список в массив numpy
+        self.vectors = np.array(vectors).astype('float32')
         self.text_names = text_names
 
+        self.index = faiss.IndexFlatL2(self.vectors.shape[1])
+        self.index.add(self.vectors)  
+
     def find_similar_text(self, user_vector):
-        similarities = cosine_similarity([user_vector], self.vectors)
-        most_similar_index = np.argmax(similarities)
+        user_vector = np.array(user_vector).astype('float32').reshape(1, -1)  # Преобразуем вектор пользователя в нужный формат
+        distances, indices = self.index.search(user_vector, 1)  # Ищем 1 ближайший сосед
+
+        most_similar_index = indices[0][0]
         most_similar_text = self.text_names[most_similar_index]
-        similarity_score = similarities[0][most_similar_index]
+        similarity_score = 1 / (1 + distances[0][0])  # Преобразуем расстояние в коэффициент схожести (0-1)
+
         return most_similar_text, similarity_score
