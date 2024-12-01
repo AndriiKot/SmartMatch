cb7b3f967c74a41ba1d4a5891628b61592662ea8
AndriiKot <a_kocyuba@ukr.net>
2024-12-01 10:42:49 +0100

    App: 'SmartMatch' 'OOP-style' ! create folder for testiog 'data_for_testing' !

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ class TextSimilarityFinder:
         self.max_words = max_words
         self.texts = load_texts(data_folder, files_source)
         self.text_names = list(self.texts.keys())
-        # self.vectors = np.array([self.get_vector(text) for text in self.texts.values()])
+        self.vectors = np.array([self.get_vector(text) for text in self.texts.values()])
 
     def get_vector(self, text):
         return get_vector(self.model, self.tokenizer, text, self.max_words)
 
     def get_user_input_vector(self):
         user_input = input("Please enter the text you want to find a similar document for: ")
-        # return self.get_vector(user_input)
+        return self.get_vector(user_input)
+    def run(self):
+        user_vector = self.get_user_input_vector()
+        similar_text_name, similarity_score = 'some_name', 0.0
+        print(f"The most similar document is: {similar_text_name} with a similarity score of {similarity_score:.4f}")
+
 
 
 
@@ -29,4 +34,4 @@ if __name__ == "__main__":
     similarity_finder = TextSimilarityFinder(
         model_name="distilbert-base-uncased",
         **{key: vars(parse_arguments())[key] for key in ['data_folder', 'files_source', 'max_words']}
-    ).get_user_input_vector()
+    )
